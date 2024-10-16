90e44ba7f818094bbf5f14e0bcea7c7f431cee49
dmitei <dima9990860349@icloud.com>
2024-10-16 11:06:02 +0300

    add some func

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 
+int ** createMatrix (size_t m, size_t n)
+{
+  // code
+}
+
+void dt (int ** mtx, size_t m, size_t n)
+{
+  //code
+}
+
 int main()
 {
   size_t m = 0, n = 0;
   std::cin >> m >> n;
+  int ** mtx = createMatrix(m, n);
   std::cout << m << " " << n;
+  dt(mtx,m,n);
 }
