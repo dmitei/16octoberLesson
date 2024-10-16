dc623028c8fbc126b83587a4aab3e74e6f18f43a
dmitei <dima9990860349@icloud.com>
2024-10-16 12:33:41 +0300

    input error & exceptions

--- main.cpp
+++ main.cpp
@@ -6,13 +6,19 @@ int main()
 {
   size_t m = 0, n = 0;
   std::cin >> m >> n;
+  int ** mtx = nullptr;
+  if (!std::cin)
+  {
+    std::cerr << "cin error" << "\n";
+    return 1;
+  }
   try
   {
     int ** mtx = createMatrix(m, n);
   }
   catch (std::exception & e)
   {
-    std::cerr << e.what()
+    std::cerr << e.what();
   }
   std::cout << m << " " << n;
   clear(mtx,m);
