211edb3324da3682af8bb7b622215247ef58586a
dmitei <dima9990860349@icloud.com>
2024-10-16 11:20:01 +0300

    func changes

--- main.cpp
+++ main.cpp
@@ -4,8 +4,31 @@ int ** createMatrix (size_t m, size_t n)
 {
   // code
 }
+void input(int ** mtx, size_t m, size_t n)
+{
+  for (size_t i = 0; i < m; ++i)
+  {
+    for (size_t j = 0; j < n; ++j)
+    {
+      std::cin >> mtx[i][j];
+    }
+  }
+}
 
-void dt (int ** mtx, size_t m, size_t n)
+void output(const int ** mtx, size_t m, size_t n)
+{
+  for (size_t i = 0; i < m; ++i)
+  {
+    std::cout << mtx[i][0];
+    for (size_t j = 1; j < n; ++j)
+    {
+      std::cout << " " << mtx[i][j];
+    }
+    std::cout << "\n";
+  }
+}
+
+void clear(int ** mtx, size_t m, size_t n)
 {
   //code
 }
@@ -16,5 +39,5 @@ int main()
   std::cin >> m >> n;
   int ** mtx = createMatrix(m, n);
   std::cout << m << " " << n;
-  dt(mtx,m,n);
+  clear(mtx,m,n);
 }
